15112cf207bdf2701c1c31b2c8e3349459b2604f
Deepak1308-KP <deepakkp1308@gmail.com>
2026-08-21 22:32:37 +0530

    21/08/25

---
+++ Enumerate.py
@@ -0,0 +1,14 @@
+# fruits=["apple", "Orange", "Banana"]
+# for index, fruits in enumerate(fruits):
+#     print(index, fruits)
+    
+##Starts with change index=1
+# fruits=["apple", "Orange", "Banana"]
+# for index, fruits in enumerate(fruits, start=1):
+#     print(index, fruits)
+    
+# num=[12, 34, 56, 67, 89, 98, 99]
+# for index, num in enumerate(num):
+#     if index==3:
+#         print("Deepak")
+#     print(index, num)
