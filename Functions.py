f5e1d52b5bebc5386010e97a72749f2b1d605508
Deepak1308-KP <deepakkp1308@gmail.com>
2026-09-10 18:56:10 +0530

    Functions

--- Functions.py
+++ Functions.py
@@ -66,4 +66,70 @@
 
 # def bank(bonus, blance):
 #     print(f"your total bank blance is {bonus+blance}")
-# bank(100, 220)
+# bank(100, 220)  
+
+##Positional Argument
+# argument assigned according to the position
+
+##Keyword argumnet
+#Argument are passed using parameter and here order it is not matter
+# def print_scores(score, name):
+#     print(f"i am {name} and i got {score} scores points")
+# print_scores(name="Deepak", score=90)
+
+##Default Argument
+# parameter have a default value, and
+# it will be override by passing another name or value
+
+# def print_scores(score, name="Deepak"):
+#     print(f"i am {name} and i got {score} scores points")
+# print_scores(80,"Deepak K P")
+
+
+# ==>Variable length argument
+
+# *args is used in a Python function to accept any number of positional
+# arguments. The arguments are stored inside a tuple.
+# *args allows a function to accept any number of positional arguments.
+
+
+# def numbers(*num):
+#     print(num)
+# numbers(1,2,3,4,5,6,7,8,9)
+
+# **kwargs is used in a Python function to accept any
+# number of keyword arguments. The arguments are stored inside a dictionary.
+# **kwargs allows a function to accept any number of keyword arguments
+
+# def students(**details):
+#     print(details)
+# students(name="Deepak", Education="B.E", Native="Shivamogga")
+
+
+# Order of execution if pass all in one prgm
+# 1. Normal positional arguments
+#           ↓
+# 2. *args (extra positional arguments)
+#           ↓
+# 3. Keyword / keyword-only arguments
+#           ↓
+# 4. Default Keyword
+#           |
+# 5. **kwargs (extra keyword arguments)
+
+# def print_scores(num1,*scores, num2:str="Deepak", num3, **number):
+#     print(scores)
+#     print(number)
+#     print(num1)
+#     print(num2)
+#     print(num3)
+# print_scores(67, 71,69, num3=72, year=2021, month=8)
+
+# def check_num(num):
+#     if num>0:
+#         return "+ve"
+#     elif num<0:
+#         return "-ve"
+#     else:
+#         return 0
+# print(check_num(0))
